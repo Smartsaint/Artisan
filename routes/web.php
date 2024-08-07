b50f76aace29682377b9718b390a49e0af7f741c
ikechukwu-unegbu <iunegbu94@yahoo.com>
2024-08-07 03:11:20 +0100

    M

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\Admin\V1\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/system/home', [HomeController::class, 'home']);
