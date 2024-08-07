c620d1854bfd2a53ed48d1a1018dd891ca92f1f3
HarryKode <danharryharry002@gmail.com>
2024-08-07 23:37:58 +0100

    Ui for Auth pages

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\View;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/login', function(){
+    return view('auth.login');
+});
+Route::get('/register', function(){
+    return view('auth.register');
+});
+Route::get('/reset-password', function(){
+    return view('auth.reset_password');
+});
+Route::get('/new-password', function(){
+    return view('auth.new_password');
 });
